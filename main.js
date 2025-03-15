7464c9d5d92b58c59aa17c5ba0502bcee485ca2b
Nirmalya Nayak <n.nayak263@gmail.com>
2025-03-15 08:59:28 +0530

    refactor media tag handling and volume adjustment logic

--- main.js
+++ main.js
@@ -1,36 +1,37 @@
-const MIN_PLAYBACKTIME = 0.1;
+const MIN_PLAYBACK_TIME = 0.1;
 const MIN_VOLUME_THRESHOLD = 0.005;
 const DEFAULT_VOLUME = 0.005;
 
-function findMediaTags() {
+function findMediaTagsAndAdjustVolume() {
   const mediaElements = document.querySelectorAll("video, audio");
 
   mediaElements.forEach((media) => {
-    if (
-      !media.paused ||
-      media.muted ||
-      (media.currentTime > MIN_PLAYBACKTIME &&
-        media.volume > MIN_VOLUME_THRESHOLD)
-    ) {
-      return;
-    }
+    if (isMediaPlayingOrLowVolume(media)) return;
 
     media.volume = DEFAULT_VOLUME;
   });
 }
 
-findMediaTags();
+function isMediaPlayingOrLowVolume(media) {
+  return (
+    !media.paused ||
+    media.muted ||
+    (media.currentTime > MIN_PLAYBACK_TIME &&
+      media.volume > MIN_VOLUME_THRESHOLD)
+  );
+}
+
+findMediaTagsAndAdjustVolume();
 
 const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
+  for (const mutation of mutations) {
     if (mutation.type === "childList") {
-      findMediaTags();
+      findMediaTagsAndAdjustVolume();
     }
-  });
+  }
 });
-const config = { childList: true, subtree: true };
 
-observer.observe(document, config);
+observer.observe(document.body, { childList: true, subtree: true });
 
 function handleKeyDown(event) {
   const video = document.querySelector("video");
@@ -105,4 +106,9 @@ function requestFullscreen(video) {
     video.webkitRequestFullscreen?.() ||
     video.mozRequestFullScreen?.() ||
     video.msRequestFullscreen?.();
+}
+
+function cleanupEventListeners() {
+  document.removeEventListener("keydown", handleKeyDown);
+  observer.disconnect();
 }
