68c4706227dcae0f2536ddd9b7ccc4389e90877d
Nirmalya Nayak <n.nayak263@gmail.com>
2025-03-15 08:58:24 +0530

    add audio track toggle functionality for video elements

--- main.js
+++ main.js
@@ -32,6 +32,34 @@ const config = { childList: true, subtree: true };
 
 observer.observe(document, config);
 
+function toggleAudioTrack(video) {
+  if (!video.audioTracks || video.audioTracks.length <= 1) return;
+
+  const { currentTime, paused } = video;
+  const enabledTrackIndex = findEnabledTrackIndex(video.audioTracks);
+  const nextTrackIndex =
+    enabledTrackIndex === -1
+      ? 0
+      : (enabledTrackIndex + 1) % video.audioTracks.length;
+
+  if (enabledTrackIndex !== -1) {
+    video.audioTracks[enabledTrackIndex].enabled = false;
+  }
+  video.audioTracks[nextTrackIndex].enabled = true;
+
+  video.currentTime = currentTime;
+  if (!paused) video.play();
+}
+
+function findEnabledTrackIndex(audioTracks) {
+  for (let index = 0; index < audioTracks.length; index++) {
+    if (audioTracks[index].enabled) {
+      return index;
+    }
+  }
+  return -1;
+}
+
 function toggleFullscreen(video) {
   if (isFullscreen()) {
     exitFullscreen();
